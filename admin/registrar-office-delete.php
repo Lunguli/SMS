81848be29cafda067897d2a3f1d5af4ae42b6825
Lunguli <125440606+Lunguli@users.noreply.github.com>
2024-02-04 18:19:15 +0300

    Add files via upload

---
+++ admin/registrar-office-delete.php
@@ -0,0 +1,35 @@
+<?php  
+session_start();
+if(isset($_SESSION['admin_id']) && 
+isset($_SESSION['role'])        &&
+isset($_GET['r_user_id'])){
+
+    if($_SESSION['role'] == 'Admin'){
+        include "../DB_connection.php";
+        include "data/registrar-office.php";
+
+        $id = $_GET['r_user_id'];
+
+        if(removeR_user($id, $conn)){
+            $sm = "Successfully deleted!";
+            header("Location: registrar-office.php?success=$sm");
+            exit;
+
+        }else{
+            $em = "unknown error occurred";
+            header("Location: registrar-office.php?error=$em");
+            exit;
+
+        }
+
+    }else{
+    header("Location: registrar-office.php");
+    exit;
+   }
+
+   }else{
+     header("Location: registrar-office.php");
+     exit;
+}
+
+?>
