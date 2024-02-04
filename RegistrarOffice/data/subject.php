86365e381d1f41d8db1b11c30848257ead40f5e9
Lunguli <125440606+Lunguli@users.noreply.github.com>
2024-02-04 19:22:17 +0300

    Add files via upload

---
+++ RegistrarOffice/data/subject.php
@@ -0,0 +1,29 @@
+<?php  
+//get all Subjects
+function getAllSubjects($conn){
+    $sql = "SELECT * FROM subject";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    if($stmt->rowCount() >= 1){
+        $subjects = $stmt->fetchAll();
+        return $subjects;
+    }else{
+        return 0;
+    }
+}
+
+//Get Subjects by ID
+function getSubjectById($subject_id, $conn){
+    $sql = "SELECT * FROM subject WHERE subject_id=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$subject_id]);
+
+    if($stmt->rowCount() == 1){
+        $subject = $stmt->fetch();
+        return $subject;
+    } else {
+        return 0;
+    }
+}
+
+?>
