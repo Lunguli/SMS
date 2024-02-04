791ed55af0e574a742fe7907e2d9e1a60f131f1f
Lunguli <125440606+Lunguli@users.noreply.github.com>
2024-02-04 20:00:45 +0300

    Add files via upload

---
+++ Teacher/req/teacher-change.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+if(isset($_SESSION['teacher_id']) && 
+isset($_SESSION['role'])){
+
+    if($_SESSION['role'] == 'Teacher'){
+        
+
+if(isset($_POST['old_pass']) &&
+   isset($_POST['new_pass'])   &&
+   isset($_POST['c_new_pass'])) {
+
+    include "../../DB_connection.php";
+    include "../data/student.php";
+
+
+    $old_pass = $_POST['old_pass'];
+    $new_pass = $_POST['new_pass'];
+    $c_new_pass = $_POST['c_new_pass'];
+
+    $teacher_id = $_SESSION['teacher_id'];
+
+    if(empty($old_pass)){
+        $em = "Old password is required";
+        header("Location: ../pass.php?perror=$em");
+        exit;
+    }else if(empty($new_pass)){
+        $em = "New Password is required";
+        header("Location: ../pass.php?perror=$em");
+        exit;
+    
+    }else if(empty($c_new_pass)){
+        $em = "Confirm New Password is required";
+        header("Location: ../pass.php?perror=$em");
+        exit;
+    
+    }else if($new_pass !== $c_new_pass){
+        $em = "New Password and Confirm Password does not match";
+        header("Location: ../pass.php?perror=$em");
+        exit;
+
+    }else if(!studentPasswordVerify($old_pass, $conn, $teacher_id)){
+        $em = "Incorrect old password";
+        header("Location: ../pass.php?perror=$em");
+        exit;
+
+    }else{
+        // hashing the password
+       $new_pass = password_hash($new_pass, PASSWORD_DEFAULT);
+       $new_pass === $new_pass;
+        $sql = "UPDATE teachers SET password = ?
+                WHERE teacher_id=?";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$new_pass, $teacher_id ]);
+
+        $sm = "The password has been changed Successfully!";
+        header("Location: ../pass.php?psuccess=$sm");
+        exit;
+       
+    }
+    
+
+}else{
+    $em = "An error occurred";
+    header("Location: ../pass.php?error=$em");
+    exit;
+}
+
+}else{
+    header("Location: ../../logout.php");
+    exit;
+}
+
+}else{
+     header("Location: ../../logout.php");
+     exit;
+}
+
+
+
+?>
